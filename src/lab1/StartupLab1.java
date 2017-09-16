eecb5fd4d7c78cd54cbe7653eaa828b655b26fe5
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-15 19:43:19 -0500

    Answer questions from lab1 readme.txt with comment in StartupLab1

--- src/lab1/StartupLab1.java
+++ src/lab1/StartupLab1.java
@@ -9,6 +9,20 @@ package lab1;
  * @author Isaac
  */
 public class StartupLab1 {
+    /*
+    * Creating an abstract Course class is a better solution than a concrete class.
+    * Since there will be some courses that do not require prerequisites, creating
+    * an abstract method to then be implemented by each class that extends Course
+    * makes the most sense. A concrete superclass would not allow for the same
+    * flexibility in handling prerequisites.
+    * When it comes to the Liskov Substitution Principle, the same idea of
+    * flexibility applies. For example, if a method takes a Course as a parameter,
+    * if you declare a variable course1 as Course course1 = new JavaCourse(), you
+    * should not have any problems. Doing JavaCourse course1 = new JavaCourse()
+    * is more rigid, as JavaCourse is a more specific kind of Course. I see an
+    * advantage to this, as portability is always a plus in programming. Code
+    * reuse makes the software development process more efficient.
+    */
     public static void main(String[] args) {
         Course intro2Prog = new IntroToProgrammingCourse("Intro to Programming", "111-111", 2.0);
         Course intro2Java = new IntroJavaCourse("Intro to Java", "222-222", 4.0, "Intro to Programming");
