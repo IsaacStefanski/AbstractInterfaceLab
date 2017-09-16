9d4cb8f73038cb6032579414538eacdcd4b87e7b
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-15 20:46:18 -0500

    Clean up lab2

--- src/lab2/StartupLab2.java
+++ src/lab2/StartupLab2.java
@@ -11,18 +11,19 @@ package lab2;
  */
 public class StartupLab2 {
     /*
-    * Creating an abstract Course class is a better solution than a concrete class.
-    * Since there will be some courses that do not require prerequisites, creating
-    * an abstract method to then be implemented by each class that extends Course
-    * makes the most sense. A concrete superclass would not allow for the same
-    * flexibility in handling prerequisites.
-    * When it comes to the Liskov Substitution Principle, the same idea of
-    * flexibility applies. For example, if a method takes a Course as a parameter,
-    * if you declare a variable course1 as Course course1 = new JavaCourse(), you
-    * should not have any problems. Doing JavaCourse course1 = new JavaCourse()
-    * is more rigid, as JavaCourse is a more specific kind of Course. I see an
-    * advantage to this, as portability is always a plus in programming. Code
-    * reuse makes the software development process more efficient.
+    * Choosing an interface affords the greatest amount of flexibility. You do
+    * have to remember to put the properties in the right subclass(es) since an
+    * an interface can only have the common behaviors/methods instead. With an
+    * interface, you can handle anything any way you see fit. True, you have no
+    * choice but to implement all the methods of an interface, but you are in
+    * complete control over what those methods do. One point worth adding is that
+    * there is a significantly reduced risk of getting stuck with bad code, a
+    * risk that is high when extending a concrete class and could be there, but
+    * to a lesser degree, when extending an abstract class.
+    * The Liskov Substitution Principle in this part of the lab is similar to the
+    * first part in that it increases the flexibility. Here, though, trying to
+    * do a behavior on an instance of IntroJavaCourse is not going to work, but
+    * it would if it was declared using Course courseName = new IntroJavaCourse().
     */
     public static void main(String[] args) {
         Course intro2Prog = new IntroToProgrammingCourse("Intro to Programming", "111-111", 2.0);
@@ -36,5 +37,12 @@ public class StartupLab2 {
         System.out.println(intro2Prog.toString());
         System.out.println(intro2Java.toString());
         System.out.println(advJava.toString());
+        
+        //Trying to give a prerequisite to Course intro2Java will work...
+        intro2Java.setPrerequisites("AP Computer Science");
+        System.out.println(intro2Java.getPrerequisites());
+        //...but trying to give a prerequisite to a specific type of Course does not
+        IntroJavaCourse introJava2 = new IntroJavaCourse("Java 2", "222-333", 1.5, "Intro to Java");
+        System.out.println(introJava2.getPrerequisites());
     }
 }
